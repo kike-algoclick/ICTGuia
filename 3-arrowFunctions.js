d1cd9329634a6e36e24f2dc64d66f1f96a8bc0b3
kike-algoclick <aldo.barriere2026@hilasal.superate.org.sv>
2025-03-18 22:23:28 -0600

    Guia terminada

--- 3-arrowFunctions.js
+++ 3-arrowFunctions.js
@@ -5,7 +5,7 @@ console.log(suma(12,16))
 
 //Ejercicio 3.2
 
-let squareNumber = (a) => a*a
+let squareNumber = (a) => a**2
 
 console.log(squareNumber(8))
 
@@ -13,7 +13,7 @@ console.log(squareNumber(8))
 
 let array = [2, 4, 6]
 
-let duplicates = (array) => array.map((m)=>(m*2))
+let duplicates = (list) => list.map((m)=>(m*2))
 
 console.log(duplicates(array))
 
